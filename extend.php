1523a5fece3ac958309759771baf2f2c4c8fd7d4
Daniel Klabbers <daniel@klabbers.email>
2018-11-27 14:42:42 +0100

    wip backend

--- extend.php
+++ extend.php
@@ -2,15 +2,42 @@
 
 namespace Flagrow\Masquerade;
 
+use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listeners\AddClientAssets::class);
-    $events->subscribe(Listeners\AddApiRoutes::class);
-    $events->subscribe(Listeners\AddWebRoutes::class);
-    $events->subscribe(Listeners\InjectPermissions::class);
-    $events->subscribe(Listeners\InjectSettings::class);
-    $events->subscribe(Listeners\DemandProfileCompletion::class);
-    $events->subscribe(Listeners\AddUserGambits::class);
-    $events->subscribe(Listeners\AddUserBioRelationship::class);
-};
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__ . 'js/dist/forum.js')
+        ->css(__DIR__ . 'resources/less/forum.less')
+        ->route(
+            '/masquerade/configure',
+            'masquerade.profile.configure',
+            Http\Controllers\ManageProfileController::class
+        )
+        ->route(
+            '/masquerade/{username}',
+            'masquerade.profile.view',
+            Http\Controllers\ViewProfileController::class
+        ),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__ . 'js/dist/admin.js')
+        ->css(__DIR__ . 'resources/less/admin.less'),
+    (new Extend\Routes('api'))
+        ->get('/masquerade/fields', 'masquerade.api.fields.index', FieldIndexController::class)
+        ->post('/masquerade/fields/order', 'masquerade.api.fields.order', OrderFieldController::class)
+        ->post('/masquerade/fields[/{id:[0-9]+}]', 'masquerade.api.fields.create', SaveFieldController::class)
+        ->patch('/masquerade/fields[/{id:[0-9]+}]', 'masquerade.api.fields.update', SaveFieldController::class)
+        ->delete('/masquerade/fields[/{id:[0-9]+}]', 'masquerade.api.fields.delete', DeleteFieldController::class)
+
+        ->get('/masquerade/profile/{id:[0-9]+}', 'masquerade.api.profile', UserProfileController::class)
+        ->get('/masquerade/configure', 'masquerade.api.configure', UserConfigureController::class)
+        ->post('/masquerade/configure', 'masquerade.api.configure.save', UserConfigureController::class),
+    (new Extend\Locales(__DIR__.'resources/locale')),
+    function (Dispatcher $events) {
+        $events->subscribe(Listeners\InjectPermissions::class);
+        $events->subscribe(Listeners\InjectSettings::class);
+        $events->subscribe(Listeners\DemandProfileCompletion::class);
+        $events->subscribe(Listeners\AddUserGambits::class);
+        $events->subscribe(Listeners\AddUserBioRelationship::class);
+    }
+];
--- src/Http/Middleware/DemandProfileCompletion.php
+++ src/Http/Middleware/DemandProfileCompletion.php
@@ -28,31 +28,6 @@ class DemandProfileCompletion implements MiddlewareInterface
         $this->url = $url->toRoute('masquerade.profile.configure');
     }
 
-    /**
-     * Process an incoming request and/or response.
-     *
-     * Accepts a server-side request and a response instance, and does
-     * something with them.
-     *
-     * If the response is not complete and/or further processing would not
-     * interfere with the work done in the middleware, or if the middleware
-     * wants to delegate to another process, it can use the `$out` callable
-     * if present.
-     *
-     * If the middleware does not return a value, execution of the current
-     * request is considered complete, and the response instance provided will
-     * be considered the response to return.
-     *
-     * Alternately, the middleware may return a response instance.
-     *
-     * Often, middleware will `return $out();`, with the assumption that a
-     * later middleware will return a response.
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param null|callable $out
-     * @return null|Response
-     */
     public function __invoke(Request $request, Response $response, callable $out = null)
     {
         /** @var User $actor */
