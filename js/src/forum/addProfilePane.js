8f5a7c15f24ce3f1b3396358ab83bfec9ee57fce
Clark Winkelmann <clark.winkelmann@gmail.com>
2019-06-21 23:44:37 +0200

    Hide link if users have a profile but can't see other's profiles

--- js/src/forum/addProfilePane.js
+++ js/src/forum/addProfilePane.js
@@ -1,29 +1,30 @@
 import {extend} from 'flarum/extend';
-import ProfilePane from './panes/ProfilePane';
-import ProfileConfigurePane from './panes/ProfileConfigurePane';
-import UserPage from 'flarum/components/UserPage';
+import app from 'flarum/app';
 import LinkButton from 'flarum/components/LinkButton';
+import UserPage from 'flarum/components/UserPage';
+import ProfileConfigurePane from './panes/ProfileConfigurePane';
+import ProfilePane from './panes/ProfilePane';
 
 export default function () {
     app.routes['fof-masquerade-view-profile'] = {
         path: '/masquerade/:username',
-        component: ProfilePane.component()
+        component: ProfilePane.component(),
     };
     app.routes['fof-masquerade-configure-profile'] = {
         path: '/masquerade/configure',
-        component: ProfileConfigurePane.component()
+        component: ProfileConfigurePane.component(),
     };
 
     extend(UserPage.prototype, 'navItems', function (items) {
-        console.log(app.forum.attribute('canViewMasquerade'), app.forum.attribute('canHaveMasquerade'));
-        if (app.forum.attribute('canViewMasquerade') || app.forum.attribute('canHaveMasquerade')) {
-            const user = this.user;
-            const href = app.forum.attribute('canHaveMasquerade') && app.session.user === user
+        const isOwnProfileAndCanHaveMasquerade = app.forum.attribute('canHaveMasquerade') && app.session.user.id() === this.user.id();
+
+        if (app.forum.attribute('canViewMasquerade') || isOwnProfileAndCanHaveMasquerade) {
+            const href = isOwnProfileAndCanHaveMasquerade
                 ? app.route('fof-masquerade-configure-profile')
-                : app.route('fof-masquerade-view-profile', {username: user.username()});
+                : app.route('fof-masquerade-view-profile', {username: this.user.username()});
             items.add('masquerade',
                 LinkButton.component({
-                    href: href,
+                    href,
                     children: app.translator.trans('fof-masquerade.forum.buttons.view-profile'),
                     icon: 'far fa-id-card',
                 }),
