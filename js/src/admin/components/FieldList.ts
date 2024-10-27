a5d55711aabe5b46f7009819cce9ababf515fb1f
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-10-27 12:48:16 +0100

    chore: refactor admin component

--- js/src/admin/components/FieldList.ts
+++ js/src/admin/components/FieldList.ts
@@ -1,13 +1,22 @@
-import Button from 'flarum/common/components/Button';
 import FieldEdit from './FieldEdit';
 
+import type { Vnode } from 'mithril';
+import type Field from '../../lib/models/Field';
+
+interface FieldListAttrs {
+  existing: Field[];
+  new: Field;
+  loading: boolean;
+  onUpdate: () => void;
+}
+
 export default class FieldList {
-  view(vnode) {
+  view(vnode: Vnode<FieldListAttrs>) {
     const { existing, new: newField, loading, onUpdate } = vnode.attrs;
 
     return m(
       'form.js-sortable-fields',
-      existing.map((field) => {
+      existing.map((field: Field) => {
         return m(FieldEdit, { field, loading, onUpdate });
       }),
       m(FieldEdit, { field: newField, loading, onUpdate })
