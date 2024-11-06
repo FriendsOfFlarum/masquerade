53e6732bfe245aeabde9456c04d1de864305566a
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-11-06 10:49:29 +0100

    fix: frontend route was undefined

--- js/src/forum/addProfilePane.ts
+++ js/src/forum/addProfilePane.ts
@@ -3,23 +3,11 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import LinkButton from 'flarum/common/components/LinkButton';
 import UserPage from 'flarum/forum/components/UserPage';
-import RootMasqueradePane from './panes/RootMasqueradePane';
 
 import type ItemList from 'flarum/common/utils/ItemList';
 import type Mithril from 'mithril';
 
 export default function addProfilePane() {
-  app.routes['fof-masquerade'] = {
-    path: '/u/:username/masquerade',
-    resolver: {
-      onmatch() {
-        if (!app.forum.attribute('canViewMasquerade')) throw new Error();
-
-        return RootMasqueradePane;
-      },
-    },
-  };
-
   extend(UserPage.prototype, 'navItems', function (items: ItemList<Mithril.Children>) {
     if (app.forum.attribute<boolean>('canViewMasquerade') || this.user?.canEditMasqueradeProfile()) {
       const edit = this.user?.canEditMasqueradeProfile();
