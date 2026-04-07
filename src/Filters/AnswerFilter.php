e2a364d42f4a68aed08e027dcb906d438b84b4f3
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-04-07 15:51:31 +0200

    fix search

--- src/Filters/AnswerFilter.php
+++ src/Filters/AnswerFilter.php
@@ -2,41 +2,33 @@
 
 namespace FoF\Masquerade\Filters;
 
+use Flarum\Search\Database\DatabaseSearchState;
 use Flarum\Search\Filter\FilterInterface;
-use Flarum\Search\AbstractRegexGambit;
 use Flarum\Search\SearchState;
 use Illuminate\Database\Query\Builder;
-use Illuminate\Database\Query\Expression;
 
+/** @implements FilterInterface<DatabaseSearchState> */
 class AnswerFilter implements FilterInterface
 {
-
-    public function apply(SearchState $search, $bit): bool
-    {
-        if (!$search->getActor()->hasPermission('fof.masquerade.view-profile')) {
-            return false;
-        }
-
-        return parent::apply($search, $bit);
-    }
-
     public function getFilterKey(): string
     {
         return 'answer';
     }
 
-    public function filter(SearchState $state, array|string $value, bool $negate): void
+    public function filter(SearchState $state, string|array $value, bool $negate): void
     {
-        $this->constrain($state->getQuery(), $value, $negate);
-    }
+        if (!$state->getActor()->hasPermission('fof.masquerade.view-profile')) {
+            return;
+        }
 
-    protected function constrain(\Illuminate\Database\Eloquent\Builder $query, string $actor, bool $negate): void
-    {
-        $query->whereExists(function (Builder $query) use ($bit) {
-            $query->select($query->raw(1))
+        $value = is_array($value) ? implode(' ', $value) : $value;
+        $value = trim($value, '"');
+
+        $state->getQuery()->whereExists(function (Builder $query) use ($value) {
+            $query->selectRaw('1')
                 ->from('fof_masquerade_answers')
-                ->where('users.id', new Expression('user_id'))
-                ->where('content', 'like', "%$bit%");
+                ->whereColumn('users.id', 'user_id')
+                ->where('content', 'like', "%$value%");
         }, 'and', $negate);
     }
 }
