fda60388e07e7126656d70b75b561119097ebfda
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-04-09 22:12:44 +0200

    feat: simplify sorting with dnd-kit

--- js/src/admin/components/MasqueradePage.tsx
+++ js/src/admin/components/MasqueradePage.tsx
@@ -5,13 +5,39 @@ import Button from 'flarum/common/components/Button';
 import FormSection from 'flarum/admin/components/FormSection';
 import FormSectionGroup from 'flarum/admin/components/FormSectionGroup';
 import type { Vnode, VnodeDOM } from 'mithril';
+import { DragDropManager } from '@dnd-kit/dom';
+import { isSortable, Sortable } from '@dnd-kit/dom/sortable';
 import Field from '../../lib/models/Field';
 import FieldEditModal from './FieldEditModal';
 import sortFields from '../../common/utils/sortFields';
 
 export default class MasqueradePage extends ExtensionPage {
+  protected dragDropManager!: DragDropManager;
+
   oninit(vnode: Vnode) {
     super.oninit(vnode);
+    this.dragDropManager = new DragDropManager();
+
+    this.dragDropManager.monitor.addEventListener('dragend', (event) => {
+      if (event.canceled) return;
+
+      const { source } = event.operation;
+      if (!isSortable(source)) return;
+
+      const { initialIndex, index } = source;
+      if (initialIndex === index) return;
+
+      const fields = sortFields(app.store.all<Field>('masquerade-fields'));
+      const [movedField] = fields.splice(initialIndex, 1);
+      fields.splice(index, 0, movedField);
+      const sorting: number[] = [];
+      fields.forEach((field, newIndex) => {
+        field.pushAttributes({ sort: newIndex });
+        sorting.push(Number(field.id()));
+      });
+
+      this.updateSort(sorting);
+    });
 
     if (!app.store.all<Field>('masquerade-fields').length) {
       this.loading = true;
@@ -32,7 +58,7 @@ export default class MasqueradePage extends ExtensionPage {
     return (
       <FormSectionGroup className="MasqueradePage container">
         <FormSection label={app.translator.trans('fof-masquerade.admin.fields.title')}>
-          <ol className="MasqueradePage-list">
+          <ol className="MasqueradePage-list" oncreate={this.onListCreate.bind(this)}>
             {sortFields(app.store.all<Field>('masquerade-fields')).map((field) => (
               <li key={field.id()} data-id={field.id()} className="MasqueradeFieldListItem">
                 {field.icon() && <Icon name={field.icon()} className="MasqueradeFieldListItem-icon" />}
@@ -52,6 +78,23 @@ export default class MasqueradePage extends ExtensionPage {
         </FormSection>
       </FormSectionGroup>
     );
+  }
+
+  onListCreate(vnode: VnodeDOM<ExtensionPageAttrs, this>) {
+    const elements = vnode.dom.querySelectorAll<HTMLLIElement>('.MasqueradeFieldListItem');
+    elements.forEach((element, index) => {
+      new Sortable(
+        {
+          id: element.dataset.id!,
+          index,
+          element,
+          transition: {
+            duration: 200,
+          },
+        },
+        this.dragDropManager
+      );
+    });
   }
 
   updateSort(sorting: number[]) {
